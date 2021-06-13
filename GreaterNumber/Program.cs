d7b8f473a4572d4c45ea86106a131d470630f1a0
JosephAr1 <jonielhet@yahoo.com>
2021-06-13 17:31:52 +1200

    Add project files.

---
+++ GreaterNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GreaterNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first, second, third;
+            Console.WriteLine("Enter First Number: ");
+            first = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Second number : ");
+            second = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Third number : ");
+            third = int.Parse(Console.ReadLine());
+
+            if(first> second)
+            {
+                if (first > third)
+                {
+                    Console.WriteLine("largest number : " + first);
+                }
+                else
+                {
+                    Console.WriteLine("largest number : " + third);
+                }
+            }
+            else
+            {
+                if(second> third)
+                {
+                    Console.WriteLine("Largest number : " + second);
+                }
+                else
+                {
+                    Console.WriteLine("largest number : " + third);
+                }
+            }
+        }
+    }
+}
